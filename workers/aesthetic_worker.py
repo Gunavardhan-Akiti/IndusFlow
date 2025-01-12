da139532db87b68a6e03a977140cf141bb17e7de
Gunavardhan-Akiti <gvardhan.akiti@gmail.com>
2025-01-12 01:43:04 +0000

    Entire Pipeline

---
+++ workers/aesthetic_worker.py
@@ -0,0 +1,158 @@
+import asyncio
+import base64
+import io
+import multiprocessing
+import threading
+import time
+
+import aiohttp
+from workers.kafka_utils import Consumer, Producer
+from workers.common import load_yaml_config, print_counter
+from loguru import logger
+from PIL import Image
+
+import torch
+import torch.multiprocessing as mp
+from torch.utils.data import IterableDataset, DataLoader
+from aesthetic_predictor_v2_5 import convert_v2_5_from_siglip
+from transformers import SiglipImageProcessor
+
+
+STAGE = "aesthetic_scoring"
+
+
+async def preprocess_image(image_content, preprocessor):
+    try:
+        image_data = base64.b64decode(image_content)
+        image = Image.open(io.BytesIO(image_data)).convert("RGB")
+        processed = preprocessor(images=image, return_tensors="np").pixel_values.squeeze(0)
+        return processed
+    except Exception as e:
+        logger.error(f"Error preprocessing image: {e}")
+        return None
+
+async def preprocess_images(image_contents, preprocessor):
+    tasks = [preprocess_image(image_content, preprocessor) for image_content in image_contents]
+    return await asyncio.gather(*tasks)
+
+
+class ImageDataset(IterableDataset):
+    def __init__(self, consumer_topic, rank):
+        self.consumer_topic = consumer_topic
+        self.rank = rank
+        logger.info(f"Initialized ImageDataset for rank {rank}")
+        self.consumer = None
+
+    def __iter__(self):
+        self.consumer = Consumer(self.consumer_topic, partition_id=self.rank)
+        preprocessor = SiglipImageProcessor.from_pretrained("google/siglip-so400m-patch14-384")
+        logger.info(f"Created consumer for topic {self.consumer_topic} and partition {self.rank}")
+        for message in self.consumer.consumer:
+            # logger.debug(f"Processing message with {len(message.value['image_contents'])} images")
+            message_data = message.value
+
+            document_id = message_data["document_ids"]
+            processed = asyncio.run(preprocess_images(message_data["image_contents"], preprocessor))
+        
+            # Return one image from the buffer
+            for img, doc_id in zip(processed, document_id):
+            # for img, doc_id in zip(message_data["image_contents"], message_data["document_ids"]):
+                yield {
+                    "images": img,
+                    "document_ids": doc_id
+                }
+    
+    def __del__(self):
+        if self.consumer:
+            self.consumer.consumer.close()
+
+
+class AestheticScorerWorker:
+    def __init__(self, config, rank, counter):
+        self.stage_config = config["stages"][STAGE]
+        logger.info(f"Config: {self.stage_config}")
+        
+        consumer_topic = self.stage_config["consumer"]
+
+        self.model, _ = convert_v2_5_from_siglip(
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+            use_flash_attention_2=True
+        )
+        
+        # Move model to device and convert to bfloat16 after initialization
+        n_gpus = torch.cuda.device_count()
+        device = torch.device(f"cuda:{rank % n_gpus}")
+        self.model = self.model.to(device).to(torch.bfloat16).eval()
+
+        producer_topics = self.stage_config["producer_list"]
+        self.producers = [
+            Producer(
+                producer_topic, 
+                config["kafka_topics"][producer_topic]["partitions"]
+            ) for producer_topic in producer_topics
+        ]
+
+        self.dataset = ImageDataset(consumer_topic, rank)
+        batch_size = self.stage_config["batch_size"]
+        self.dataloader = DataLoader(self.dataset, batch_size=batch_size, num_workers=1, pin_memory=True, prefetch_factor=3)
+
+        logger.info(f"Initialized model on device cuda:{rank % n_gpus}")
+        logger.info("ScorerWorker initialization complete")
+
+        self.counter = counter
+
+    def run(self):
+        logger.info("Starting scoring process")
+        for idx, batch in enumerate(self.dataloader):
+            # logger.info(f"Processing batch {idx} with {len(batch['document_ids'])} documents")
+            # try:
+            images = batch["images"].to(self.model.device, dtype=torch.bfloat16)
+            with torch.no_grad():
+                scores = self.model(images).logits.to(torch.float32).cpu().numpy()
+            # logger.debug(f"Successfully scored batch {idx}")
+            for producer in self.producers:
+                producer.send(idx, {
+                    "document_ids": batch["document_ids"],
+                    "scores": scores
+                })
+            with self.counter.get_lock():
+                self.counter.value += len(batch["document_ids"])
+            # except Exception as e:
+            #     logger.error(f"Error processing batch {idx}: {e}")
+        
+    def __del__(self):
+        if self.dataloader:
+            self.dataloader._iterator = None  # Force cleanup of iterator
+            del self.dataloader
+        if self.dataset:
+            del self.dataset
+        if self.producers:
+            for producer in self.producers:
+                producer.close()
+
+
+def score_image(rank, config, counter):
+    logger.info(f"Starting worker process with rank {rank}")
+    worker = AestheticScorerWorker(config, rank, counter)
+    worker.run()
+
+
+if __name__ == "__main__":
+    # Add this line before any other code
+    mp.set_start_method('spawn', force=True)
+    
+    config = load_yaml_config("configs/example_config.yaml")
+    stage_consumer = config["stages"][STAGE]["consumer"]
+    N_PROCESSES = config["kafka_topics"][stage_consumer]["partitions"]
+
+    counter = print_counter()
+
+    mp.spawn(
+        score_image,
+        args=(config, counter),
+        nprocs=N_PROCESSES,
+        join=True
+    )
+
+    logger.info("ScorerWorker process completed")
---
+++ workers/mongo_utils.py
@@ -0,0 +1,124 @@
+import os
+from dotenv import load_dotenv
+from torch.utils.data import IterableDataset
+from typing import Final
+from urllib.parse import quote_plus
+from pymongo import MongoClient
+from loguru import logger
+from tqdm import tqdm
+
+load_dotenv()
+
+# Iniitialize
+MONGODB_URI: Final[str] = os.environ["MONGODB_URI"]
+MONGODB_USER: Final[str] = os.environ["MONGODB_USER"]
+MONGODB_PASSWORD: Final[str] = os.environ["MONGODB_PASSWORD"]
+encoded_user = quote_plus(MONGODB_USER)
+encoded_password = quote_plus(MONGODB_PASSWORD)
+MONGODB_URI = f"mongodb+srv://{encoded_user}:{encoded_password}@{MONGODB_URI}"
+LOCAL_TEMP_DIR: Final[str] = "/dev/shm"
+
+
+class MongoDataset():
+    DB_NAME = "world_model"
+    DOC_ID_FIELD = "_id"
+    IMAGE_URL_FIELD = "s3url"
+
+    def __init__(self, collection_name, num_shards=1, shard_idx = 0):
+        self.collection_name = collection_name
+        self.num_shards = num_shards
+        self.shard_idx = shard_idx
+        self.collection = None
+        self.query = {
+            "$expr": {
+                    "$eq": [
+                        {
+                            "$mod": [
+                                {"$toLong": f"$seed"},
+                                self.num_shards,  # Total number of shards
+                            ]
+                        },
+                        self.shard_idx,  # Current shard index
+                    ]
+                }
+        } if self.num_shards > 1 else {}
+
+    def set_collection(self):
+        self.collection = MongoClient(MONGODB_URI)[self.DB_NAME][self.collection_name]
+
+    def bulkUpdate(self, operations):
+        if self.collection is None:
+            self.set_collection()
+        return self.collection.bulk_write(operations, ordered=False)
+
+
+class CC12MDataset(MongoDataset, IterableDataset):
+    """
+    Dataset for CC12M dataset
+    """
+    COLLECTION_NAME = "cc12m"
+    
+    def __init__(self, num_shards=1, shard_idx = 0):
+        super().__init__(self.COLLECTION_NAME, num_shards, shard_idx)
+
+    def __iter__(self):
+        self.set_collection()
+        self.collection.create_index([("aesthetic_score", 1)])
+        logger.info("Starting to iterate over documents")
+        self.query.update({"aesthetic_score": {"$exists": False}})
+        for doc in tqdm(self.collection.find(
+            self.query,
+        ).batch_size(128 * 1024)):
+            # logger.info(f"Processing document: {doc[self.DOC_ID_FIELD]}")
+            yield {
+                "document_ids":  str(doc[self.DOC_ID_FIELD]), 
+                "image_urls": str(doc[self.IMAGE_URL_FIELD])
+            }
+
+
+class PD12MDataset(MongoDataset, IterableDataset):
+    """
+    Dataset for PD12M dataset
+    """
+    COLLECTION_NAME = "pd12m"
+
+    def __init__(self, num_shards=1, shard_idx = 0):
+        super().__init__(self.COLLECTION_NAME, num_shards, shard_idx)
+
+    def __iter__(self):
+        self.set_collection()
+        self.collection.create_index([("aesthetic_score", 1)])
+        logger.info("Starting to iterate over documents")
+        self.query.update({"aesthetic_score": {"$exists": False}})
+        for doc in tqdm(self.collection.find(
+            self.query,
+        ).batch_size(128 * 1024)):
+            # logger.info(f"Processing document: {doc[self.DOC_ID_FIELD]}")
+            yield {
+                "document_ids": str(doc[self.DOC_ID_FIELD]), 
+                "image_urls": str(doc[self.IMAGE_URL_FIELD])
+            }
+
+
+class DiffusionDataset(MongoDataset, IterableDataset):
+    """
+    Dataset for Diffusion dataset
+    """
+    COLLECTION_NAME = "diffusion"
+
+    def __init__(self, num_shards=1, shard_idx = 0):
+        super().__init__(self.COLLECTION_NAME, num_shards, shard_idx)
+
+    def __iter__(self):
+        self.set_collection()
+        self.collection.create_index([("aesthetic_score", 1)])
+        logger.info("Starting to iterate over documents")
+        self.query.update({"aesthetic_score": {"$exists": False}})
+        for doc in self.collection.find(
+            self.query,
+        ).batch_size(32 * 1024):
+            # logger.info(f"Processing document: {doc[self.DOC_ID_FIELD]}")
+            yield {
+                "document_ids": str(doc[self.DOC_ID_FIELD]), 
+                "image_urls": str(doc[self.IMAGE_URL_FIELD])
+            }
